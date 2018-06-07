a228362f70d7b7dff1241291e261014d508cd5dd
Blake Howe <blakehowe@hotmail.com>
2018-06-07 13:13:50 +1000

    added request param to feign interface

--- src/main/java/com/ts/vendor/Service/VendorService.java
+++ src/main/java/com/ts/vendor/Service/VendorService.java
@@ -5,9 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="vendor-service")
 public interface VendorService {
     @GetMapping(value = "/page")
-    Page<Vendor> findBySearchTerm(String searchTerm, Pageable pageable);
+    Page<Vendor> findBySearchTerm(@RequestParam("search") String searchTerm, Pageable pageable);
 }
