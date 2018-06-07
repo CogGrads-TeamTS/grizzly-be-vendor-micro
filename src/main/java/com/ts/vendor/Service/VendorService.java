4477b84e15408fe7322995bca69e5a495f8896ce
Blake Howe <blakehowe@hotmail.com>
2018-06-07 15:34:50 +1000

    fix service

--- src/main/java/com/ts/vendor/Service/VendorService.java
+++ src/main/java/com/ts/vendor/Service/VendorService.java
@@ -1,14 +1,11 @@
 package com.ts.vendor.Service;
 
 import com.ts.vendor.Model.Vendor;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="vendor-service")
+
 public interface VendorService {
-    @GetMapping(value = "/page")
     Page<Vendor> findBySearchTerm(@RequestParam("search") String searchTerm, Pageable pageable);
 }
