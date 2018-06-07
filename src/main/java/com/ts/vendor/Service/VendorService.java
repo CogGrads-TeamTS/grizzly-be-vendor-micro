ea0dce93c7fa33e683beec0c39975bcbe42b2fd6
Blake Howe <blakehowe@hotmail.com>
2018-06-07 12:45:38 +1000

    added feign client

--- src/main/java/com/ts/vendor/Service/VendorService.java
+++ src/main/java/com/ts/vendor/Service/VendorService.java
@@ -1,9 +1,11 @@
 package com.ts.vendor.Service;
 
 import com.ts.vendor.Model.Vendor;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+@FeignClient(name="vendor-service")
 public interface VendorService {
 
     Page<Vendor> findBySearchTerm(String searchTerm, Pageable pageable);
