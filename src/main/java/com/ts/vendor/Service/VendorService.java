0d147a239bb2be1c61503add91695143675e4dbd
Blake Howe <blakehowe@hotmail.com>
2018-06-07 13:12:17 +1000

    added feign mapping

--- src/main/java/com/ts/vendor/Service/VendorService.java
+++ src/main/java/com/ts/vendor/Service/VendorService.java
@@ -4,9 +4,10 @@ import com.ts.vendor.Model.Vendor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name="vendor-service")
 public interface VendorService {
-
+    @GetMapping(value = "/page")
     Page<Vendor> findBySearchTerm(String searchTerm, Pageable pageable);
 }
